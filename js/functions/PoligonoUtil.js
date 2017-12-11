eaec035a34fe328d0f670725dd3f4f23277d6648
lucasszmt <lucasszmt@gmail.com>
2017-12-11 11:14:17 -0200

    Tá Pica

--- js/functions/PoligonoUtil.js
+++ js/functions/PoligonoUtil.js
@@ -716,16 +716,108 @@ function calculoDaNormal(p1, p2, p3, n) {
     }
 }
 
+/**
+ * Realiza o cálculo da normal para um dado poligono como parâmetro
+ *
+ * @param poligono
+ */
 function facesVisiveis(poligono) {
     faces_visiveis = [];
     poligono.faces.forEach(function (face) {
         console.log(face);
-        if (face.arestas.length > 0) {
-            if (calculoDaNormal(face.arestas[0].pontoA,
-                    face.arestas[0].pontoB,
-                    face.arestas[1].pontoB, v_n)) {
-                faces_visiveis.push(face);
-            }
+        // if (face.arestas.length > 0) {
+        if (calculoDaNormal(face.arestas[0].pontoA,
+                face.arestas[0].pontoB,
+                face.arestas[1].pontoB, v_n)) {
+            faces_visiveis.push(face);
         }
+        // }
     });
+}
+
+/**
+ * Verifica se q está entre p e r
+ * @param p
+ * @param q
+ * @param r
+ * @returns {boolean}
+ */
+function onSegment(p, q, r) {
+    if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+        q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
+        return true;
+    return false;
+}
+
+function orientacao(p, q, r) {
+    var val = (q.y - p.y) * (r.x - q.x) -
+        (q.x - p.x) * (r.y - q.y);
+
+    if (val == 0) return 0;  // colinear
+    return (val > 0) ? 1 : 2; // clock or counterclock wise
+}
+
+/**
+ * Verifica se duas retas se interceptam
+ *
+ * @param p1
+ * @param q1
+ * @param p2
+ * @param q2
+ * @returns {boolean}
+ */
+function intercepta(p1, q1, p2, q2) {
+    // Find the four orientations needed for general and
+    // special cases
+    var o1 = orientacao(p1, q1, p2);
+    var o2 = orientacao(p1, q1, q2);
+    var o3 = orientacao(p2, q2, p1);
+    var o4 = orientacao(p2, q2, q1);
+
+    // General case
+    if (o1 != o2 && o3 != o4)
+        return true;
+
+    // Special Cases
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+
+    // p1, q1 and p2 are colinear and q2 lies on segment p1q1
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+    // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+    return false;
+}
+
+function isInside(polygon, p) {
+
+    // Create a point for line segment from p to infinite
+    var extreme = new Ponto(Number.POSITIVE_INFINITY, p.y);
+
+    // Count intersections of the above line with sides of polygon
+    var count = 0, i = 0;
+    do {
+        var next = (i + 1) % n;
+
+        // Check if the line segment from 'p' to 'extreme' intersects
+        // with the line segment from 'polygon[i]' to 'polygon[next]'
+        if (intercepta(polygon[i], polygon[next], p, extreme)) {
+            // If the point 'p' is colinear with line segment 'i-next',
+            // then check if it lies on segment. If it lies, return true,
+            // otherwise false
+            if (orientacao(polygon[i], p, polygon[next]) == 0)
+                return onSegment(polygon[i], p, polygon[next]);
+
+            count++;
+        }
+        i = next;
+    } while (i != 0);
+
+    // Return true if count is odd, false otherwise
+    return count % 2 == 1;  // Same as (count%2 == 1)
 }
